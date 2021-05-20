be940a93c436b3d0df7099b52643e0b33caf65f0
Julien-rtg <julien.rittl@gmail.com>
2021-05-20 17:49:58 +0200

    add DL CV

--- templates/realisations.php
+++ templates/realisations.php
@@ -9,6 +9,8 @@
     <a href="<?= $router->generate('home'); ?>">Accueil</a> <!-- $router->generate(''); calling route from index.php -->
     <a href="<?= $router->generate('presentation'); ?>">Présentation</a>
     <a href="<?= $router->generate('realisations'); ?>">Réalisations</a>
+    <a href="/SitePerso/img/CV_Julien_Rittling.pdf" download="CV_Julien_Rittling">CV :
+    <img src="/SitePerso/img\downloadupdates_theapplication_thearrow_descarg_2915.png" alt=""></a>
 </div>
 <hr>
 <div class="navRealisations">
