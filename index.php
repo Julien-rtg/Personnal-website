4f8117067d320aaec2bebe27c7d4288ae2d30e1b
Julien-Rtg <julien.rittl@gmail.com>
2021-07-28 17:02:21 +0200

    style update

--- index.php
+++ index.php
@@ -8,15 +8,13 @@ $var = new Deploy(); // refer to src/deploy.php
 
 $router->setBasePath($var->switch()); // Root
 
-$router->map('GET', '/', 'templates/home.php', 'home');
-$router->map('GET', '/Realisations', 'templates/realisations.php', 'realisations');
-$router->map('GET', '/Realisations/MLS', 'templates/realisations/MLS.php', 'realisationsMLS');
+$router->map('GET', '/', 'templates/home.html', 'home');
 
 $match = $router->match();
 if ($match) {
-    require 'elements/header.php';
+    require 'elements/header.html';
     require $match['target'];
-    require 'elements/footer.php';
+    require 'elements/footer.html';
 } else {
     header("HTTP/1.0 404 Not Found");
     require '404.html';
